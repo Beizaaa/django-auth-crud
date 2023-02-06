9bb21958de5740be831063b3f4fe8d167b9930a3
Beizaaa <beizaby@gmail.com>
2023-02-06 18:51:57 -0300

    try fix with whitenoiseuser true

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -54,8 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+    ]
+
+WHITENOISE_USE_FINDERS = True
 
 ROOT_URLCONF = 'djangocrud.urls'
 
